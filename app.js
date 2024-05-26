2a4ce1ae6cea25af836c4239f8f9d2e83ab45f57
brittney betzold <brittney.betzold2022@gmail.com>
2024-05-26 18:44:16 -0500

    week14-1 Auth and Passport

--- app.js
+++ app.js
@@ -35,26 +35,23 @@ if (inProduction) {
 }
 
 app.use(session(sessionParms));
+const passport = require("passport");
+const passportInit = require("./passport/passportInit");
+
+passportInit();
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(require("connect-flash")());
+app.use(require("./middleware/storeLocals"));
 
-// secret word handling
-app.get("/secretWord", (req, res) => {
-  req.session.secretWord ||= "syzygy";
-  res.locals.info = req.flash("info");
-  res.locals.errors = req.flash("error");
-  res.render("secretWord", { secretWord: req.session.secretWord });
-});
+const secretWordRouter = require("./routes/secretWord");
+const auth = require("./middleware/auth");
+app.use("/secretWord", auth, secretWordRouter);
 
-app.post("/secretWord", (req, res) => {
-  if (req.body.secretWord?.toLowerCase()?.startsWith("p")) {
-    req.flash("error", "That word won't work!");
-    req.flash("error", "You can't use words that start with p.");
-  } else {
-    req.session.secretWord = req.body.secretWord;
-    req.flash("info", "The secret word was changed.");
-  }
-  res.redirect("/secretWord");
+app.get("/", (req, res) => {
+  res.render("index");
 });
+app.use("/sessions", require("./routes/sessionRoutes"));
 
 app.use((req, res) => {
   res.status(404).send(`That page (${req.url}) was not found.`);
@@ -69,6 +66,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await require("./db/connect")(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
---
+++ models/User.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email address"],
+
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide valid email address ",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a valid password"],
+    minlength: 6,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// UserSchema.methods.createJWT = function () {
+//   try {
+//     return jwt.sign(
+//       { userId: this._id, name: this.name },
+//       process.env.JWT_SECRET,
+//       {
+//         expiresIn: process.env.JWT_LIFETIME,
+//       }
+//     );
+//   } catch (error) {
+//     console.error("Error creating JWT:", error);
+//     throw new Error("Failed to create JWT token");
+//   }
+// };
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
+module.exports = mongoose.model("User", UserSchema);
